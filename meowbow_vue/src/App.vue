4b2965b062d166b7f91f7c6dd9b001d7c7567172
HyunWoo Oh <gusdn959@gmail.com>
2022-04-16 01:16:42 +0900

    meowbow_vue baseline code. scss setting added in vue.app

---
+++ meowbow_vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "@/assets/scss/styles";
+</style>
