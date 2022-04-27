3f1b8f6b5ec25d2b2bb9361c49ec9c4f15dc11bd
lyeong-gwa <wjdgmll77@naver.com>
2022-04-27 07:57:40 +0000

    모델적용완료

--- MeowBowWow2emoji/web/models.py
+++ MeowBowWow2emoji/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    uploadedFile = models.FileField(upload_to="image/")
+    session = models.CharField(max_length=200)
+    dateTimeOfUpload = models.DateTimeField(auto_now=True)
