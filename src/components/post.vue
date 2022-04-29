d6a7a3e4b6ae3c78643fd88a36d4233c5c438193
HyunWoo Oh <gusdn959@gmail.com>
2022-04-29 17:07:16 +0900

    implemented check box and function that uploading image using drag and drop

--- src/components/post.vue
+++ src/components/post.vue
@@ -9,19 +9,33 @@
                   show-size label="개나 고양이의 이미지를 넣어주세요!"
                   ></v-file-input>
             </v-col>
+            <v-col cols="1"> 
+              <v-checkbox
+              v-model="animal_check"
+              :label="`${animal_check.toString()}`"
+              false-value="Cat"
+              true-value="Dog"
+              hide-details
+            ></v-checkbox>
+            </v-col>
             <v-col cols="2">
-              <v-btn @click="upload" color="primary" class="ml-auto">이미지 변환하기</v-btn>
+              <v-btn @click="upload" color="indigo" class="mx-2" dark fab>
+                <v-icon dark> mdi-camera-flip </v-icon>
+              </v-btn>
             </v-col>
           </v-row>
           <v-row justify="space-around">
-            <v-col cols="auto">
-              <v-card class="mx-auto" max-width="700">
+            <v-col cols="4">
+              <v-card class="mx-auto" max-width="700" v-cloak @drop.prevent="addDropFile" @dragover.prevent>
                 <v-img :src="url" contain id="img1" height="300px" width='300px' class="mx-auto"></v-img>
-              <v-card-title> 개 또는 고양이의 이미지를 올려주세요! </v-card-title>
+              <v-card-title class="justify-center"> 이미지를 드래그 해주세요! </v-card-title>
               </v-card>
             </v-col>  
-          <v-col cols="auto">
-            <v-img :src="url" height="300px" width='300px' class="ml-auto" />
+          <v-col cols="4">
+            <v-card class="mx-auto" max-width="700">
+              <v-img id="img2" height="300px" width='300px' class="mx-auto" />
+              <v-card-title class="justify-center">내가 출력되었다냥!</v-card-title>
+            </v-card>
           </v-col>  
           </v-row>
         </v-row>
@@ -38,14 +52,22 @@ export default {
     data: () => ({
     //
     files: [],
+    animal_check: 'Cat',  
     url: null,
     image: null
       }),
 
   methods: {
+    addDropFile(e) {
+       this.image = e.dataTransfer.files[0]; 
+       this.url= URL.createObjectURL(this.image)
+       console.log(this.image)},
+       
+
     async upload() {
       var fd = new FormData();
       fd.append('pet_images', this.image);
+      fd.append('animal', this.animal_check);
       await axios.post('http://3.26.152.53/emoji/',
           fd, {
             headers: {
@@ -59,6 +81,7 @@ export default {
         })
         .catch(function () {
           console.log("Fail");
+          console.log(fd)
         });
     },
     Preview_image() {
