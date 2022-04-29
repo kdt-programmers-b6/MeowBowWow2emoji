f409ddef5ae706eb70c913f5129fded8029cc9ff
HyunWoo Oh <gusdn959@gmail.com>
2022-04-29 18:19:53 +0900

    integrated vue file

--- nginx/src/components/post.vue
+++ nginx/src/components/post.vue
@@ -1,36 +1,74 @@
 <template>
     <v-container>
-        <v-file-input v-model="image" @change="Preview_image" show-size label="Image"></v-file-input>
-        <v-btn color="green" @click="add_image">아래 이미지 추가하기</v-btn>
-        <v-btn @click="upload" color="primary">이미지 변환하기</v-btn>
-        <v-img :src="url" contain height="400px" width="400px"></v-img>
-        <img id="img2" />
+        <v-row>
+          <v-row>
+            <v-col cols="5">
+              <v-file-input v-model="image"
+                  prepend-icon="mdi-camera"
+                  @change="[Preview_image(), add_image()]"
+                  show-size label="개나 고양이의 이미지를 넣어주세요!"
+                  ></v-file-input>
+            </v-col>
+            <v-col cols="1"> 
+              <v-checkbox
+              v-model="animal_check"
+              :label="`${animal_check.toString()}`"
+              false-value="Cat"
+              true-value="Dog"
+              hide-details
+            ></v-checkbox>
+            </v-col>
+            <v-col cols="2">
+              <v-btn @click="upload" color="indigo" class="mx-2" dark fab>
+                <v-icon dark> mdi-camera-flip </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row justify="space-around">
+            <v-col cols="4">
+              <v-card class="mx-auto" max-width="700" v-cloak @drop.prevent="addDropFile" @dragover.prevent>
+                <v-img :src="url" contain id="img1" height="300px" width='300px' class="mx-auto"></v-img>
+              <v-card-title class="justify-center"> 이미지를 드래그 해주세요! </v-card-title>
+              </v-card>
+            </v-col>  
+          <v-col cols="4">
+            <v-card class="mx-auto" max-width="700">
+              <v-img id="img2" height="300px" width='300px' class="mx-auto" />
+              <v-card-title class="justify-center">내가 출력되었다냥!</v-card-title>
+            </v-card>
+          </v-col>  
+          </v-row>
+        </v-row>
     </v-container>
 </template>
-
 <script>
 import axios from 'axios'
-
 
 export default {
     // eslint-disable-next-line
     name: 'post',
-    components: {
+    components: { 
     },
     data: () => ({
     //
     files: [],
+    animal_check: 'Cat',  
     url: null,
     image: null
       }),
 
   methods: {
+    addDropFile(e) {
+       this.image = e.dataTransfer.files[0]; 
+       this.url= URL.createObjectURL(this.image)
+       console.log(this.image)},
+       
+
     async upload() {
       var fd = new FormData();
-      for(let i=0; i<this.files.length; ++i){
-        fd.append('pet_images', this.files[i]);
-        }
-      await axios.post('http://3.26.152.53/emoji/',
+      fd.append('pet_images', this.image);
+      fd.append('animal', this.animal_check);
+      await axios.post('http://3.26.152.53:8000/emoji/',
           fd, {
             headers: {
               'Content-Type': 'multipart/form-data',
@@ -43,6 +81,7 @@ export default {
         })
         .catch(function () {
           console.log("Fail");
+          console.log(fd)
         });
     },
     Preview_image() {
@@ -50,13 +89,14 @@ export default {
     },
     add_image() {
       console.log(this.image)
-      this.files.push(this.image)
-      console.log(this.files)
-    }
-}
+    },
+  }
 }
 </script>
 
-<style>
-  
+<style scoped>
+  img1 {
+    height: 400;
+    width: 400;
+  }
 </style>
