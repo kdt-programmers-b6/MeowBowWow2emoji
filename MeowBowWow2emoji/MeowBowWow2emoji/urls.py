9373bcaa321b034b3428039c41cc8b9bfed6b02f
lyeong-gwa <wjdgmll77@naver.com>
2022-05-02 10:09:44 +0000

    "마무리 직전"

--- MeowBowWow2emoji/MeowBowWow2emoji/urls.py
+++ MeowBowWow2emoji/MeowBowWow2emoji/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import main_page,emoji_api
+from web.views import main_page, emoji_api
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', main_page),
-    path('emoji/',emoji_api),   
+    path('emoji/', emoji_api),   
     path('admin/', admin.site.urls),
 ]
 
