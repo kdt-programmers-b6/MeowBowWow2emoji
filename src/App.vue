d25bb5c3e1e18f84844c5ba9cf8abdb9bc37da9c
HyunWoo Oh <gusdn959@gmail.com>
2022-05-02 16:54:51 +0900

    image hover fuction added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <v-main>
       <background />
       <post />
+      <h3 align="center">예시 이미지들 입니다.</h3>
+      <ShowExam />
     </v-main>
   </v-app>
 </template>
@@ -10,12 +12,14 @@
 <script>
 import background from './components/background.vue'
 import post from './components/post.vue'
+import ShowExam from './components/ShowExam.vue'
 
 export default {
   name: 'App',
   components: {
     background,
     post,
+    ShowExam,
   }
 };
 </script>
---
+++ src/components/ShowExam.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="700" @mouseover="changeImageWhen('over')" @mouseout="changeImageWhen('out')" >
+      <v-img :src="require('@/assets/sample_img/' + current_img1)" height="400px" width='400px' class="mx-auto" />
+      <v-card-title class="justify-center">마우스를 올려주세요!</v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'show_example',
+  data: () => ({
+    mouse_event: {
+      over: "1_fake.jpg",
+      out: "1_real.jpg",
+    },
+    current_img1: "1_real.jpg"
+ }),
+ methods: {
+   changeImageWhen (event) {
+     this.current_img1 = this.mouse_event[event]
+  }
+ }
+}
+</script>
